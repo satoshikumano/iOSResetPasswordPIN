0b56ebf919ffe69ef38482f65d9d77dc36402cb8
satoshi <satoshi.kumano@kii.com>
2016-06-29 15:57:13 +0900

    Initial Commit

---
+++ PasswordResetTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PasswordResetTest
+//
+//  Created by satoshi on 2016/06/29.
+//  Copyright © 2016年 Kii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PasswordResetTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PasswordResetTest
+//
+//  Created by satoshi on 2016/06/29.
+//  Copyright © 2016年 Kii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
